d3eeab121e78506233a165a46d531af6d880a4fb
Дмитрий <farming-2@mail.ru>
2025-02-23 21:52:44 +0300

    added class Task

---
+++ TaskManager/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public Task(string description)
+        {
+            Description = description;
+            IsCompleted = false;
+        }
+    }
+}
